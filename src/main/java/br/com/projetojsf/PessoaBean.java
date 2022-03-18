88c2c856a4982ddc9c2377c61fe71453ed4eac23
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-03-18 12:38:47 -0300

    Redirecionamento de URL em JSP

--- src/main/java/br/com/projetojsf/PessoaBean.java
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -23,7 +23,7 @@ public class PessoaBean {
 		
 		if(nomes.size() > 3) {
 			commandButton.setDisabled(true);
-			return "paginanavegada";
+			return "paginanavegada?faces-redirect=true";
 		}
 		
 		return ""; // null ou vazio fica na mesma página -> outcome
